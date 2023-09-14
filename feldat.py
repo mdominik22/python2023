7e034453a5c8c0b8f7230dc0d0dd968602a55c37
Makarész Dániel <makareszd22@ADY-NAGYATAD.SULINET.HU>
2023-09-14 10:24:03 +0200

    feladat

---
+++ feldat.py
@@ -0,0 +1,11 @@
+a = 6
+b = 2
+
+
+if a == b:
+    print ("egyenlő")
+elif a > b:
+    print ("A")
+else:
+    print ("B")
+           
---
+++ valtozok.py
@@ -0,0 +1,28 @@
+#változókkal kapcsolatos gyakorlások
+
+szam=112
+print(szam)
+
+szoveg="almafa"
+print(szoveg)
+
+szoveg='Géza kék\taz\ nég'
+print(szoveg)
+
+vanEPenzeGergonek=False
+print(vanEPenzeGergonek)
+
+
+szam=5+6
+print(szam)
+szam=szam+5
+print(szam)
+szam=szam+5
+print(szam)
+
+print(szam+1)
+print(szam*2)
+print(szam-1)
+print(szam/2)
+
+print(12//13)
