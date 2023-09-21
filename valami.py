c05c864b8e90ef7e493607f190f5a38b627e16f7
Makarész Dániel <makareszd22@ADY-NAGYATAD.SULINET.HU>
2023-09-21 13:17:45 +0200

    feladat

--- valami.py
+++ valami.py
@@ -1,4 +1,26 @@
-i=7
+i=5
 
-for i in range (9,100,):
+for i in range (10,100,10):
     print(i, end="\t")
+    
+for i in range(5,100):
+    if i %10==0:
+        print(i)
+for i in range (10):
+    print(i*i)
+    
+print("---------------")
+for i in range (0,100):
+    if 100 <= i*i:
+        if 999 >= i*i:
+            print(i*i)
+print("---------------")
+for i in range (1,11):
+        print(i*2)
+        
+
+
+    
+        
+        
+    
