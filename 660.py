091fcf5bd98e2f90a45a3845570a5fb65e2caec3
mdominik22 <120101316+mdominik22@users.noreply.github.com>
2024-01-13 13:42:42 +0100

    Update 660.py

--- 660.py
+++ 660.py
@@ -1,11 +1,3 @@
-karaktersorozat = input("Adjon meg egy karaktersorozatot: ")
-
-szamokdarabja = sum(1 for karakter in karaktersorozat if karakter.isdigit())
-
-print(f"A megadott karaktersorozatban {szamokdarabja} db szám karakter van.")
-
-print(70*"-")
-
 karakterek = input("Adjon meg egy karaktersorozatot: ")
 
 szamdarab = 0
@@ -14,7 +6,4 @@ for karakter in karakterek:
         szamdarab += 1
 
 print(f"A megadott karaktersorozatban {szamdarab} db szám karakter van.")
-
-
-
 
