90e942bac30a8ddcb6e0dbbadcb5b7aa349d6df6
mdominik22 <120101316+mdominik22@users.noreply.github.com>
2024-01-13 13:53:37 +0100

    Add files via upload

---
+++ 512.py
@@ -0,0 +1,17 @@
+print("Makarész Dominik")
+print:("10B")
+print("2024.01.13")
+
+import math
+
+while True:
+    try:
+        szam = float(input("Adjon meg egy számot (0 végjelig): "))
+    except ValueError:
+        print("Érvénytelen bemenet. Csak számokat adjon meg.")
+    if szam > 0:
+        negyzetgyok = math.sqrt(szam)
+        print(f"A(z) {szam} négyzetgyöke: {negyzetgyok}")
+    else:
+        print("Helytelen szám a számnak pozitívnak kell lennie.")
+
