8ce183def6c1be8b77419ba245715edd49d74e97
mdominik22 <120101316+mdominik22@users.noreply.github.com>
2024-01-14 13:31:18 +0100

    Update 512.py

--- 512.py
+++ 512.py
@@ -8,10 +8,14 @@ while True:
     try:
         szam = float(input("Adjon meg egy számot (0 végjelig): "))
     except ValueError:
-        print("Ez nem szám, Csak számokat adjon meg.")
+        print("Érvénytelen bemenet. Csak számokat adjon meg.")
+        continue
+    if szam ==0:
+        print("Miért adnád meg ezt a számot? Adj meg másikat ami nem 0-val kezdődik!!")
+        break
     if szam > 0:
         negyzetgyok = math.sqrt(szam)
         print(f"A(z) {szam} négyzetgyöke: {negyzetgyok}")
     else:
-        print("Helytelen szám, a számnak pozitívnak kell lennie.")
+        print("Helytelen szám a számnak pozitívnak kell lennie.")
 
